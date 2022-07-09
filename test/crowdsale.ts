59af9a224e1a976470c3eac86525f501a663ceed
MikMik1011 <mikmikyt1011@gmail.com>
2022-07-09 20:16:54 +0200

    add tests for withdraw, first one fails idk why

--- test/crowdsale.ts
+++ test/crowdsale.ts
@@ -29,7 +29,7 @@ describe("Crowdsale", () => {
         expect(crowdsale.address).to.properAddress;
     });
 
-    describe.skip("buy", async () => {
+    describe("buy", async () => {
             
             it("should register buy-in", async () => {
                 await crowdsale.buy({ value: ethers.utils.parseEther("1") });
@@ -52,7 +52,7 @@ describe("Crowdsale", () => {
             await ethers.provider.send("evm_mine", []);
             await crowdsale.collectEth();
             expect(await ethers.provider.getBalance(crowdsale.address)).to.eq(ethers.utils.parseEther("0"));
-            //TODO: check if amount is transferred to owner 
+            //TODO: check if that amount is transferred to owner, idk how to do that cuz gas 
             //expect(await ethers.provider.getBalance(signers[0].address)).to.eq(ethers.utils.parseEther("1"));
         });
 
@@ -64,6 +64,34 @@ describe("Crowdsale", () => {
         it("should revert if not owner of contract", async () => {
             crowdsale = await crowdsale.connect(signers[1]);
             await expect(crowdsale.collectEth()).to.be.revertedWith(generateErrorMsg("Ownable: caller is not the owner"));
+        });
+    });
+
+    describe("withdraw", async() => {
+        it.skip("should withdraw tokens and distribute them evenly", async () => {
+            await crowdsale.buy({ value: ethers.utils.parseEther("2") });
+            let crowdsale1 = await crowdsale.connect(signers[1]);
+            await crowdsale1.buy({ value: ethers.utils.parseEther("6") });
+            await ethers.provider.send("evm_increaseTime", [(3 * 24 * 60 * 60) + 1]);
+            await ethers.provider.send("evm_mine", []);
+            await crowdsale.withdraw();
+            // the line below fails idk why
+            await expect(BigNumber.from(crowdsale.balanceOf(signers[0].address))).to.eq(ethers.utils.parseEther("1"));
+            await crowdsale1.withdraw();
+            await expect(crowdsale1.balanceOf(signers[1].address)).to.eq(ethers.utils.parseEther("3"));
+        });
+
+        it("should revert on second withdraw attempt", async () => {
+            await crowdsale.buy({ value: ethers.utils.parseEther("1") });
+            await ethers.provider.send("evm_increaseTime", [(3 * 24 * 60 * 60) + 1]);
+            await ethers.provider.send("evm_mine", []);
+            await crowdsale.withdraw();
+            await expect(crowdsale.withdraw()).to.be.revertedWith(generateErrorMsg("You have already withdrew your tokens!"));
+        });
+
+        it("should revert during buy-in phase", async () => {
+            await crowdsale.buy({ value: ethers.utils.parseEther("1") });
+            await expect(crowdsale.withdraw()).to.be.revertedWith(generateErrorMsg("Buy-in phase is not finished!"));
         });
     });
 });

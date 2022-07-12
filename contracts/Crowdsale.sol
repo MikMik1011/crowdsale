c7478884c1639c81f7951d690e5cbe104e2710d1
MikMik1011 <mikmikyt1011@gmail.com>
2022-07-12 18:24:32 +0200

    optimize and organize contract

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -11,14 +11,45 @@ contract Crowdsale is ERC20, Ownable {
     mapping(address => bool) internal collected;
 
     constructor(uint256 total) ERC20("Crowdsale", "CRS") {
-        _mint(address(this), total * (10 ** decimals()));
+        _mint(address(this), total * (10**decimals()));
         creationTime_ = block.timestamp;
     }
 
     function buy() external payable {
-        require(block.timestamp <= creationTime_ + 3 days, "Buy-in phase is over!");
-        buyins[msg.sender] += msg.value;
-        totalBuyin += msg.value;
+        require(
+            block.timestamp <= creationTime_ + 3 days,
+            "Buy-in phase is over!"
+        );
+
+        unchecked {
+            buyins[msg.sender] += msg.value;
+            totalBuyin += msg.value;
+        }
+    }
+
+    function collectEth() external onlyOwner {
+        require(
+            block.timestamp > creationTime_ + 3 days,
+            "Buy-in phase is not finished!"
+        );
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function withdraw() external {
+        require(
+            block.timestamp > creationTime_ + 3 days,
+            "Buy-in phase is not finished!"
+        );
+        require(
+            !collected[msg.sender],
+            "You have already withdrew your tokens!"
+        );
+        collected[msg.sender] = true;
+        _transfer(
+            address(this),
+            msg.sender,
+            (buyins[msg.sender] * totalSupply()) / totalBuyin
+        );
     }
 
     function getBuyin(address _addr) external view returns (uint256) {
@@ -28,17 +59,4 @@ contract Crowdsale is ERC20, Ownable {
     function getTotalBuyin() external view returns (uint256) {
         return totalBuyin;
     }
-
-    function collectEth() external onlyOwner {
-        require(block.timestamp > creationTime_ + 3 days, "Buy-in phase is not finished!");
-        payable(msg.sender).transfer(address(this).balance);
-    }
-
-    function withdraw() external {
-        require(block.timestamp > creationTime_ + 3 days, "Buy-in phase is not finished!");
-        require(!collected[msg.sender], "You have already withdrew your tokens!");
-        collected[msg.sender] = true;
-        _transfer(address(this), msg.sender, buyins[msg.sender] * totalSupply() / totalBuyin);
-    }
 }
-
